db926ee08569b932c65eaab276d71fb8f470d549
sureshranwa989-svg <sureshranwa989@gmail.com>
2026-05-22 18:37:07 +0530

    Update API_URL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:3000";
+export const API_URL = "https://bug-backend-chi.vercel.app";
 
 const API = axios.create({
     baseURL: `${API_URL}/api`,
